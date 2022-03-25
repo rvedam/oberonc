c433eb314181ceab8b9a0d231dde34fec4d7c619
Ram Vedam <rvedam@gmail.com>
2022-03-25 01:15:10 -0400

    changed main implementation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include "CstVisitor.hpp"
+#include "llvm/IR/IRBuilder.h"
+
+#include "antlr4-runtime.h"
+#include "OberonLexer.h"
+#include "OberonParser.h"
 #include "CstVisitor.hpp"
 
 int main(int argc, char** argv) 
 {
+  std::string filename;
+  auto debugMode = false;
+  // read the stream
+  if(argc < 2) {
+    std::cout << "USAGE: ./oberonc [-d] <filenames>..." << std::endl;
+    return -1;
+  }
+
+  if(std::string{argv[1]} == "-d") 
+  {
+    filename = std::string{argv[2]};
+    debugMode = true;
+  }
+  else 
+    filename = std::string{argv[1]};
+  
   return 0;
 }
